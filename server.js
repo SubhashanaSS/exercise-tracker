e1f4dc2d966f6bb23db8761942031ffda53e12b9
SubhashanaSS <subhashana201@gmail.com>
2022-04-05 23:11:08 +0530

    models/routes

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT||5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri)
-.then(()=> {
-    console.log('Database Connection Successful');
-})
-.catch((err)=> console.log('Database Connection Error',err));
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch((err) => console.log("Database Connection Error", err));
 
+ const exercisesRouter = require('./routes/exercises');
+ const usersRouter = require('./routes/users');
 
-app.listen(port,()=>{
-    console.log(`Server is running on port: ${port}`);
+ app.use('/exerciss', exercisesRouter);
+ app.use('/users', usersRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
 });
