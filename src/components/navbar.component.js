a02a9102f15257d355ebf6d909a819a03e094d5d
SubhashanaSS <subhashana201@gmail.com>
2022-04-12 23:37:07 +0530

    NavBar Complete

---
+++ src/components/navbar.component.js
@@ -0,0 +1,26 @@
+import React, {Component} from "react";
+import {Link} from 'react-router-dom';
+
+export default class Navbar extends Component{
+
+    render(){
+        return(
+            <nav classname="navbar navbar-dark bg-dark navbar-expand-lg">
+                <Link to="/" className="navbar-brand">Exercise Tracker</Link>
+                <div className="collpse navbar-collapse">
+                <ul className="navbar-nav mr-auto">
+                    <li className="navbar-item">
+                    <Link to="/" className="nav-link">Exercises</Link>
+                    </li>
+                    <li className="navbar-item">
+                    <Link to="/create" className="nav-link">Create Exercises Log</Link>
+                    </li>
+                    <li className="navbar-item">
+                    <Link to="/" className="nav-link">Create User</Link>
+                    </li>
+                </ul>
+                </div>
+            </nav>
+        );
+    }
+}
